9b3642b733106a28bbf578d802e16ef56138e1e7
YONES <107154559+ynstf@users.noreply.github.com>
2025-11-20 01:00:49 +0100

    change

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ python manage.py collectstatic --noinput
 
 # Run migrations (optional, but recommended)
 echo "Running migrations..."
+python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
 /opt/ENVCRM/bin/gunicorn --worker-tmp-dir /dev/shm base.wsgi:application --bind "0.0.0.0:${APP_PORT}"
